ee79f7166efd4d11d4e15bc041ac65ac8293cecd
Shoshana Berleant <berleant@stanford.edu>
2017-01-20 22:42:39 +0000

    add scratch var scope with reuse==False

--- ops.py
+++ ops.py
@@ -10,6 +10,14 @@ from tensorflow.python.framework import ops
 
 from utils import *
 
+# create scratch variable scope to get around reuse=True issues with
+# temporary variables
+# (hack around https://github.com/tensorflow/tensorflow/issues/5827)
+with tf.variable_scope("scratch", reuse=False) as scratch_varscope:
+    SCRATCH_VARSCOPE = scratch_varscope
+assert SCRATCH_VARSCOPE == scratch_varscope
+assert SCRATCH_VARSCOPE.reuse == False
+
 class batch_norm(object):
     """Code modification of http://stackoverflow.com/a/33950177"""
     def __init__(self, epsilon=1e-5, momentum = 0.9, name="batch_norm"):
@@ -29,13 +37,13 @@ class batch_norm(object):
                                     initializer=tf.constant_initializer(0.))
                 self.gamma = tf.get_variable("gamma", [shape[-1]],
                                     initializer=tf.random_normal_initializer(1., 0.02))
-
-                batch_mean, batch_var = tf.nn.moments(x, [0, 1, 2], name='moments')
-                ema_apply_op = self.ema.apply([batch_mean, batch_var])
-                self.ema_mean, self.ema_var = self.ema.average(batch_mean), self.ema.average(batch_var)
-
-                with tf.control_dependencies([ema_apply_op]):
-                    mean, var = tf.identity(batch_mean), tf.identity(batch_var)
+                with tf.variable_scope(SCRATCH_VARSCOPE) as scratch_varscope:
+                    assert scratch_varscope.reuse == False
+                    batch_mean, batch_var = tf.nn.moments(x, [0, 1, 2], name='moments')
+                    ema_apply_op = self.ema.apply([batch_mean, batch_var])
+                    self.ema_mean, self.ema_var = self.ema.average(batch_mean), self.ema.average(batch_var)
+                    with tf.control_dependencies([ema_apply_op]):
+                        mean, var = tf.identity(batch_mean), tf.identity(batch_var)
         else:
             mean, var = self.ema_mean, self.ema_var
 
