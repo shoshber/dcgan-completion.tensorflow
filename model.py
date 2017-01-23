074af90108fed0060dae19e5175b422c1565f967
Shoshana Berleant <berleant@stanford.edu>
2017-01-23 02:44:57 +0000

    save pictures more often

--- model.py
+++ model.py
@@ -203,7 +203,7 @@ Initializing a new one.
                     % (epoch, idx, batch_idxs,
                         time.time() - start_time, errD_fake+errD_real, errG))
 
-                if np.mod(counter, 100) == 1:
+                if np.mod(counter, 10) == 0:
                     samples, d_loss, g_loss = self.sess.run(
                         [self.sampler, self.d_loss, self.g_loss],
                         feed_dict={self.z: sample_z, self.images: sample_images}
